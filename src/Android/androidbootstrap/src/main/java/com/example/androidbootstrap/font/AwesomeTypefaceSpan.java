95744f5daa1bb68ff160a0e30d107bfaac1cbe53
MunSeongUk <almighty911216@gmail.com>
2017-06-13 03:29:26 +0900

    Lib update

---
+++ src/Android/androidbootstrap/src/main/java/com/example/androidbootstrap/font/AwesomeTypefaceSpan.java
@@ -0,0 +1,38 @@
+package com.example.androidbootstrap.font;
+
+import android.content.Context;
+import android.os.Parcel;
+import android.support.annotation.NonNull;
+import android.text.TextPaint;
+import android.text.style.TypefaceSpan;
+
+import com.example.androidbootstrap.TypefaceProvider;
+
+
+/**
+ * A custom span which paints text using the typeface specified by the IconSet passed to the constructor
+ */
+public class AwesomeTypefaceSpan extends TypefaceSpan {
+
+    private final Context context;
+    private final IconSet iconSet;
+
+    public AwesomeTypefaceSpan(Context context, IconSet iconSet) {
+        super(iconSet.fontPath().toString());
+        this.context = context.getApplicationContext();
+        this.iconSet = iconSet;
+    }
+
+    @Override public void updateDrawState(@NonNull TextPaint ds) {
+        ds.setTypeface(TypefaceProvider.getTypeface(context, iconSet));
+    }
+
+    @Override public void updateMeasureState(@NonNull TextPaint paint) {
+        paint.setTypeface(TypefaceProvider.getTypeface(context, iconSet));
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+    }
+}
